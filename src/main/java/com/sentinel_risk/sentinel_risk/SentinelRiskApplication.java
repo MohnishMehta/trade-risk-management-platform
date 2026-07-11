2d8acc16ca657d27702bf3854dbd2541a88a65c7
MohnishMehta <mohnishwork31@gmail.com>
2026-07-11 16:23:29 -0400

    Intial commit

---
+++ src/main/java/com/sentinel_risk/sentinel_risk/SentinelRiskApplication.java
@@ -0,0 +1,13 @@
+package com.sentinel_risk.sentinel_risk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentinelRiskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SentinelRiskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sentinel_risk/sentinel_risk/SentinelRiskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sentinel_risk.sentinel_risk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SentinelRiskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
